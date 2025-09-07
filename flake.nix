0398b641ec68d0f6ab0a7aec0be1926a424b3876
Sinedka <123822966+Sinedka@users.noreply.github.com>
2025-09-08 00:26:12 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,60 +1,98 @@
 {
-  description = "Desktop shell for Caelestia dots";
+  rev,
+  lib,
+  python3,
+  installShellFiles,
+  swappy,
+  libnotify,
+  slurp,
+  wl-clipboard,
+  cliphist,
+  app2unit,
+  dart-sass,
+  grim,
+  fuzzel,
+  wl-screenrec,
+  dconf,
+  killall,
+  caelestia-shell,
+  withShell ? false,
+  discordBin ? "discord",
+  qtctStyle ? "Darkly",
+}:
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+python3.pkgs.buildPythonApplication {
+  pname = "caelestia-cli";
+  version = rev;
+  src = ./.;
+  pyproject = true;
 
-    quickshell = {
-      url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+  build-system = with python3.pkgs; [
+    hatch-vcs
+    hatchling
+  ];
 
-    caelestia-cli = {
-      url = "github:caelestia-dots/cli";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.caelestia-shell.follows = "";
-    };
-  };
+  dependencies = with python3.pkgs; [
+    materialyoucolor
+    pillow
+  ];
 
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  } @ inputs: let
-    forAllSystems = fn:
-      nixpkgs.lib.genAttrs nixpkgs.lib.platforms.linux (
-        system: fn nixpkgs.legacyPackages.${system}
-      );
-  in {
-    formatter = forAllSystems (pkgs: pkgs.alejandra);
+  pythonImportsCheck = ["caelestia"];
 
-    packages = forAllSystems (pkgs: rec {
-      caelestia-shell = pkgs.callPackage ./nix {
-        rev = self.rev or self.dirtyRev;
-        stdenv = pkgs.clangStdenv;
-        quickshell = inputs.quickshell.packages.${pkgs.system}.default.override {
-          withX11 = false;
-          withI3 = false;
-        };
-        app2unit = pkgs.callPackage ./nix/app2unit.nix {inherit pkgs;};
-        caelestia-cli = inputs.caelestia-cli.packages.${pkgs.system}.default;
-      };
-      with-cli = caelestia-shell.override {withCli = true;};
-      debug = caelestia-shell.override {debug = true;};
-      default = caelestia-shell;
-    });
+  nativeBuildInputs = [installShellFiles];
+  propagatedBuildInputs = [
+    swappy
+    libnotify
+    slurp
+    wl-clipboard
+    cliphist
+    app2unit
+    dart-sass
+    grim
+    fuzzel
+    wl-screenrec
+    dconf
+    killall
+  ] ++ lib.optional withShell caelestia-shell;
 
-    devShells = forAllSystems (pkgs: {
-      default = let
-        shell = self.packages.${pkgs.system}.caelestia-shell;
-      in
-        pkgs.mkShell.override {stdenv = shell.stdenv;} {
-          inputsFrom = [shell shell.plugin shell.extras];
-          packages = with pkgs; [material-symbols rubik nerd-fonts.caskaydia-cove];
-          CAELESTIA_XKB_RULES_PATH = "${pkgs.xkeyboard-config}/share/xkeyboard-config-2/rules/base.lst";
-        };
-    });
+  SETUPTOOLS_SCM_PRETEND_VERSION = 1;
 
-    homeManagerModules.default = import ./nix/hm-module.nix self;
+  patchPhase = ''
+    # Replace qs config call with nix shell pkg bin
+    substituteInPlace src/caelestia/subcommands/shell.py \
+    	--replace-fail '"qs", "-c", "caelestia"' '"caelestia-shell"'
+    substituteInPlace src/caelestia/subcommands/screenshot.py \
+    	--replace-fail '"qs", "-c", "caelestia"' '"caelestia-shell"'
+
+    # Use config bin instead of discord + fix todoist
+    substituteInPlace src/caelestia/subcommands/toggle.py \
+    	--replace-fail 'discord' ${discordBin} \
+      --replace-fail 'todoist' 'todoist.desktop'
+
+    # Use config style instead of darkly
+    substituteInPlace src/caelestia/data/templates/qtct.conf \
+    	--replace-fail 'Darkly' '${qtctStyle}'
+  '';
+
+  # --- Новый installPhase для автоматической установки бинарника ---
+  installPhase = ''
+    # стандартная установка Python приложения
+    python3 -m pip install . --prefix=$out
+
+    # убедимся, что исполняемый файл появился в $out/bin
+    mkdir -p $out/bin
+    cp $out/lib/python*/site-packages/caelestia/cli.py $out/bin/caelestia
+    chmod +x $out/bin/caelestia
+
+    # установим completions
+    installShellCompletion completions/caelestia.fish
+  '';
+
+  meta = {
+    description = "The main control script for the Caelestia dotfiles";
+    homepage = "https://github.com/caelestia-dots/cli";
+    license = lib.licenses.gpl3Only;
+    mainProgram = "caelestia";
+    platforms = lib.platforms.linux;
   };
 }
